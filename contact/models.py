c2a9e01e9e0b0edf2d8266486dbb233010ee3552
Caue <crburgardt@gmail.com>
2023-09-18 16:41:04 -0300

    create contact model and migretions

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Contact(models.Model):
+    first_name = models.CharField(max_length=40)
+    last_name = models.CharField(max_length=40)
+    phone = models.CharField(max_length=20, blank=True)
+    email = models.EmailField(max_length=254)
+    created_date = models.DateTimeField(default=timezone.now)
+    description = models.TextField(blank=True)
+
