ba97babd610d3d59c28107f97ed09b07dd716a32
Caue <crburgardt@gmail.com>
2023-09-19 15:34:42 -0300

    add media and static to database

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from contact import models
 
-# Register your models here.
+
+@admin.register(models.Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = 'id', 'first_name', 'last_name', 'email',
+    ordering = '-id',
+    list_filter = 'created_date', 'first_name',
+    search_fields = 'id', 'first_name', 'last_name',
+    list_per_page = 10
+    list_max_show_all = 100
+    list_editable = 'first_name', 'last_name',
+    list_display_links = 'id', 'email',
