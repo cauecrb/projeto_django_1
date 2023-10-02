7670fc7c8bea13a796ca798f1d57574111d77466
Caue <crburgardt@gmail.com>
2023-10-02 16:36:35 -0300

    migration with foreignkey category

--- contact/models.py
+++ contact/models.py
@@ -2,6 +2,10 @@ from django.db import models
 from django.utils import timezone
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+
 class Contact(models.Model):
     first_name = models.CharField(max_length=40)
     last_name = models.CharField(max_length=40)
@@ -11,6 +15,8 @@ class Contact(models.Model):
     description = models.TextField(blank=True)
     show = models.BooleanField(default=True)
     picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/')
+    #category = models.CharField(max_length=50)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True)
 
     def __str__(self) -> str:
         return f'{self.first_name} {self.last_name}'
