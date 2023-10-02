298aed3e90756a9780620057c8c5c673bb6b8025
Caue <crburgardt@gmail.com>
2023-10-02 17:15:58 -0300

    create owner

--- contact/models.py
+++ contact/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 
 # Create your models here.
@@ -25,6 +26,7 @@ class Contact(models.Model):
     picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/')
     #category = models.CharField(max_length=50)
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True)
+    owner = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True)
 
     def __str__(self) -> str:
         return f'{self.first_name} {self.last_name}'
