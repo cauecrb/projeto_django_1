842adc80a9aa759487fa7bb292cde06a63c1eda4
Caue <crburgardt@gmail.com>
2023-10-03 12:33:27 -0300

    create local_settings and css

--- projeto_django_1/settings.py
+++ projeto_django_1/settings.py
@@ -131,3 +131,8 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try:
+    from projeto_django_1.local_settings import *
+except ImportError:
+    ...
