2cef1243e0df22075072d6e5f3365986c6833aae
Caue <crburgardt@gmail.com>
2023-11-21 18:58:39 -0300

    Create search contacts

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render, get_object_or_404
 from contact.models import Contact
+from django.shortcuts import redirect
+from django.db.models import Q
 
 def index(request):
     contacts = Contact.objects.filter(show=True).order_by('-id')
 
     context = {
         'contacts': contacts,
+        'site_title': 'Contatos - ',
     }
 
     return render(
@@ -20,12 +23,43 @@ def contact(request, contact_id):
 
     single_contact = get_object_or_404(Contact, pk=contact_id, show=True)
 
+    site_title = f'{single_contact.first_name} {single_contact.last_name} - '
+
     context = {
         'contact': single_contact,
+        'site_title': site_title,
     }
 
     return render(
         request,
         'contact/pages/contact.html',
+        context
+    )
+
+
+def search(request):
+    #single_contact = Contact.objects.get(pk=contact_id )
+    search_value = request.GET.get('q','').strip()
+
+    if search_value == '':
+        return redirect('contact:index')
+
+    print(search_value)
+
+    contacts = Contact.objects \
+        .filter(show=True) \
+        .filter(Q(first_name__contains=search_value))\
+        .order_by('-id')
+    print(contacts.query)
+
+    context = {
+        'contacts': contacts,
+        'site_title': 'Search - ',
+    }
+    print(context)
+
+    return render(
+        request,
+        'contact/pages/index.html',
         context
     )
