066f1f655b669bb6bd13e83ab3b3f9fa77c41024
Caue <crburgardt@gmail.com>
2023-12-01 17:04:03 -0300

    create dimnamic form

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -3,10 +3,42 @@ from contact.models import Contact
 from django.shortcuts import redirect
 from django.db.models import Q
 from django.core.paginator import Paginator
+from django.core.exceptions import ValidationError
+from django import forms
+
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = Contact
+        fields = (
+            'first_name', 'last_name', 'phone', 'email',
+        )
+
+    def clean(self):
+        cleaned_data = self.cleaned_data
+
+        self.add_error(
+            None, ValidationError(
+                'mensagem de erro', code='invalid'
+            )
+        )
+
+        return super().clean()
 
 def create(request):
+    if request.method == 'POST':
+        context = {
+            'form': ContactForm(request.POST)
+        }
+
+        return render(
+            request,
+            'contact/pages/create.html',
+            context
+        )
+
 
     context = {
+        'form': ContactForm()
     }
 
     return render(
