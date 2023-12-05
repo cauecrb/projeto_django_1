3ccd755c4c586acfb79b445b94955a82313b5259
Caue <crburgardt@gmail.com>
2023-12-05 17:43:55 -0300

    save contact in DB

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,12 +1,18 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from contact.forms import ContactForm
 
 
 def create(request):
     if request.method == 'POST':
+        form = ContactForm(request.POST)
         context = {
-            'form': ContactForm(request.POST)
+            'form': form
         }
+
+        if form.is_valid():
+            contact = form.save()
+            contact.save()
+            return redirect('contact:create')
 
         return render(
             request,
