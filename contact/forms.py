ccdf5a3e943b11e4f34a7f6585533f60bb66a6eb
Caue <crburgardt@gmail.com>
2023-12-07 18:04:23 -0300

    add first, last name and email field in adm user form

--- contact/forms.py
+++ contact/forms.py
@@ -1,6 +1,7 @@
 from django.core.exceptions import ValidationError
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 from . import models
 
@@ -99,4 +100,30 @@ class ContactForm(forms.ModelForm):
         return first_name
 
 class RegisterForm(UserCreationForm):
-    ...
+    first_name = forms.CharField(
+        required=True,
+    )
+    last_name = forms.CharField(
+        required=True,
+    )
+    email = forms.EmailField(
+        required=True,
+    )
+
+    class Meta:
+        model = User
+        fields = (
+            'first_name', 'last_name', 'email',
+            'username', 'password1', 'password2',
+        )
+
+    def clean_email(self):
+        email = self.clean_email.get('email')
+
+        if User.objects.filter(email=email).exists():
+            self.add_error(
+                'email',
+                ValidationError('Este email já está em uso', code='existsemail')
+            )
+
+        return email
--- contact/views/user_forms.py
+++ contact/views/user_forms.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from contact.forms import RegisterForm
 
+
 def register(request):
     form = RegisterForm()
 
