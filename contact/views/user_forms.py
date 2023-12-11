2f41a488183fd36f35f80cfe03e32bad0536dac7
Caue <crburgardt@gmail.com>
2023-12-11 17:32:45 -0300

    create login/logout user

--- contact/views/user_forms.py
+++ contact/views/user_forms.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from contact.forms import RegisterForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import auth
 
 
 def register(request):
@@ -12,7 +14,7 @@ def register(request):
         if form.is_valid():
             form.save()
             messages.success(request, 'Usuário salvo com sucesso!')
-            return redirect('contact:index')
+            return redirect('contact:login')
 
 
     return render(
@@ -23,3 +25,27 @@ def register(request):
         }
     )
 
+def login_view(request):
+    form = AuthenticationForm(request)
+
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+
+        if form.is_valid():
+            user = form.get_user()
+            auth.login(request, user)
+            print(user)
+            messages.success(request, 'Logado com sucesso!')
+            return redirect('contact:index')
+
+    return render(
+        request,
+        'contact/pages/login.html',
+        {
+            'form': form
+        }
+    )
+
+def logout_view(request):
+    auth.logout(request)
+    return redirect('contact:login')
