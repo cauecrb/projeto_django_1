95033b8ff712ccc4d047606fc62329cfe00fd4b1
Caue <crburgardt@gmail.com>
2023-12-12 17:22:51 -0300

    show only contacts for owner

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -19,7 +19,8 @@ def create(request):
         }
 
         if form.is_valid():
-            contact = form.save()
+            contact = form.save(commit=False)
+            contact.owner = request.user
             contact.save()
             return redirect('contact:update', contact_id=contact.id)
 
@@ -44,7 +45,7 @@ def create(request):
 @login_required(login_url='contact:login')
 def update(request, contact_id):
     contact = get_object_or_404(
-        Contact, id=contact_id, show=True
+        Contact, id=contact_id, show=True, owner=request.user
     )
     form_action = reverse('contact:update', args=(contact_id,))
 
@@ -81,7 +82,7 @@ def update(request, contact_id):
 @login_required(login_url='contact:login')
 def delete(request, contact_id):
     contact = get_object_or_404(
-        Contact, id=contact_id, show=True
+        Contact, id=contact_id, show=True, owner=request.user
     )
     #contact.delete()
     #return redirect('contact:index')
